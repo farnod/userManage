dfd3d462fe03a13b7b6a66a3efb1ad0e09326d72
farnoud <farnoodcc@gmail.com>
2023-05-01 11:46:08 +0430

    add customer page add

--- components/template/AddCustomerPage.js
+++ components/template/AddCustomerPage.js
@@ -1,9 +1,23 @@
-import Form from "../module/Form"
+import Form from "../module/Form";
+import { useState } from "react";
 
 function AddCustomerPage() {
-  return (
-    <div><Form /></div>
-  )
+  const [form, setForm] = useState({
+    name: "",
+    lastName: "",
+    email: "",
+    phone: "",
+    address: "",
+    postalCode: "",
+    date: "",
+    products: [],
+  });
+	return (
+		<div>
+			<h4>add customer page</h4>
+			<Form form={form} setForm={setForm} />
+		</div>
+	);
 }
 
-export default AddCustomerPage
+export default AddCustomerPage;
